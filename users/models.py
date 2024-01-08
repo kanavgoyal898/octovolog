8b8695c456d2a4a3fa91745e0bca0527f5a1ec22
Kanav Goyal <kanavgoyal898@gmail.com>
2024-01-08 15:46:32 +0530

    fixed_image_resizing

--- users/models.py
+++ users/models.py
@@ -6,10 +6,10 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='Profile.jpeg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def save(self):
-        super.save()
+        super().save()
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
